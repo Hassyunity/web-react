e170736d43bb19679c93d268ddcc8d501f2f9c6c
HassyUnity <hassy.tsihoarana@gmail.com>
2023-06-19 18:35:40 +0300

    add stylsheet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
+import logo from './7.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App image-container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo round-border" alt="logo" />
       </header>
     </div>
   );
